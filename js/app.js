eafa6baa2a121ddb87d88f25677edfbbf74e35ae
vlad-solomon <48878155+vlad-solomon@users.noreply.github.com>
2020-03-26 19:23:51 +0200

    [HTML] Slider [CSS] Styled slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
-	console.log("hello")
+	var mySwiper = new Swiper(".swiper-container" , {
+		speed: 400,
+		slidesPerView: "auto",
+		navigation:{
+			nextEl: ".button-next",
+			prevEl: ".button-prev"
+		},
+		keyboard:{
+			enabled: true,
+			onlyInViewport: true,
+		}
+	})
 
 });
